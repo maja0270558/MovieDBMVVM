161dac2caedddace1ec9aeea984b70dbf2ebff37
django <maja0270558@gmail.com>
2023-12-19 13:20:19 +0800

    Initial Commit

---
+++ MoiveDBMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoiveDBMVVM
+//
+//  Created by DjangoLin on 2023/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
