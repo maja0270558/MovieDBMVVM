1459d68199f6d33ffed4c106a3a55d259b8e46d2
django <maja0270558@gmail.com>
2024-01-05 12:44:33 +0800

    Create file MovieViewModelTest

---
+++ MoiveDBMVVMTests/Features/MovieViewModelTest.swift
@@ -0,0 +1,14 @@
+//
+//  MovieViewModelTest.swift
+//  MoiveDBMVVMTests
+//
+//  Created by DjangoLin on 2024/1/5.
+//
+
+@testable import MoiveDBMVVM
+import XCTest
+
+final class MovieViewModelTest: XCTestCase {
+
+   
+}
